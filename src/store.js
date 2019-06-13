18b42b4aed7869b5a8bd939431fd4bbd97e86c15
djsrv <dservilla@gmail.com>
2019-06-13 16:05:50 -0400

    Colored text in help screens

--- src/store.js
+++ src/store.js
@@ -1888,13 +1888,14 @@ SnapSerializer.prototype.loadHelpScreenElement = function (
         if (element.children.length === 0) {
             morph = screen.createParagraph(
                 element.contents.trim().split(/\s+/).join(' '),
-                textSize, textColor, italic
+                textSize, element.attributes.color || textColor, italic
             );
         } else {
             morph = screen.createRichParagraph(null, textSize, textColor);
             morph.text = element.children.map(function (child) {
                 return myself.loadHelpScreenElement(
-                    child, screen, target, textColor, italic
+                    child, screen, target,
+                    element.attributes.color || textColor, italic
                 );
             });
             morph.drawNew();
