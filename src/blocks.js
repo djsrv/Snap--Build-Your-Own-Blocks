6e63090d84fa33f71c6dd2b3e8eed63f668031f4
djsrv <dservilla@gmail.com>
2019-08-04 14:39:53 -0400

    Fix to last commit

--- src/blocks.js
+++ src/blocks.js
@@ -2167,20 +2167,12 @@ SyntaxElementMorph.prototype.showBubble = function (
     );
     if (forHelpScreen) {
         return bubble;
-   }
-   bubble.popUp(
-       wrrld,
-       pos,
-       isClickable
-   );
-        bubble.popUp(
-            wrrld,
-            pos,
-            isClickable
-        );
-    } else {
-        return bubble;
     }
+    bubble.popUp(
+        wrrld,
+        pos,
+        isClickable
+    );
     if (exportPic) {
         this.exportPictureWithResult(bubble);
     }
